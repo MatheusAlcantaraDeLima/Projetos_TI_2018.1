a1c9433782e6f17abcebee046920045c16bfc4c2
matheus <matheusalcantaradelima1997@gmail.com>
2019-11-08 15:32:00 -0300

    todos os projetos integradores foram adicionados

---
+++ mycartionic/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
+  { path: 'pagentrega', loadChildren: './pagentrega/pagentrega.module#PagentregaPageModule' },
+  { path: 'pagsaldo', loadChildren: './pagsaldo/pagsaldo.module#PagsaldoPageModule' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
